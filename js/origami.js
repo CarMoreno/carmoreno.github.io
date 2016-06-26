0064beda998756bba4a64c87f22d276e9ef113d0
carmoreno <carlosandresmorenovelez@gmail.com>
2016-06-26 17:53:59 -0500

    new section: galería :arrow_upper_right:

--- js/origami.js
+++ js/origami.js
@@ -7,6 +7,15 @@ var $elephant = $('.elephant-fold').oriDomi({
 var $clavel = $('.clavel-fold').oriDomi({
   	hPanels: 6
 });
+var $rat = $('.rat-fold').oriDomi({
+	hPanels: 8
+});
+var $lion = $('.lion-fold').oriDomi({
+	hPanels: 4
+});
+
 $rabbit.oriDomi('accordion', -30);
 $elephant.oriDomi('stairs', -18);
-$clavel.oriDomi('reveal', 20, 'top');
+$clavel.oriDomi('reveal', 30, 'top');
+$rat.oriDomi('stairs', -15);
+$lion.oriDomi('fracture', -1.5);
